8474b7bcfb94448ce236ce02e19637c1fbb1edaa
FChapeau <francois.drouinm@gmail.com>
2014-12-03 13:42:08 -0500

    Added javafx gradle plugin and ripermi library binary to repo. Note to self: check for alternative way to include ripermi into project.

--- src/main/java/client/startView/StartViewController.java
+++ src/main/java/client/startView/StartViewController.java
@@ -7,7 +7,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import net.sf.lipermi.handler.CallHandler;
 
 import java.io.IOException;
 
@@ -45,7 +44,5 @@ public class StartViewController {
 
         Stage currentStage = (Stage)ConnectionStringBox.getScene().getWindow();
         currentStage.close();
-
-        CallHandler callHandler;
     }
 }
