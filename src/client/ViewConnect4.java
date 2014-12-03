cbcd44feb07062efa232202b40094336bf556a3c
Francois Boivin <boivin.92@gmail.com>
2014-12-02 20:45:52 -0500

    Setup

---
+++ src/client/ViewConnect4.java
@@ -0,0 +1,7 @@
+package client;
+
+/**
+ * Created by boivi_000 on 2014-12-02.
+ */
+public class ViewConnect4 {
+}
---
+++ src/server/Model.java
@@ -0,0 +1,7 @@
+package server;
+
+/**
+ * Created by boivi_000 on 2014-12-02.
+ */
+public class Model {
+}
