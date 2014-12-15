1242f0a8c18d5c4e9e884ca495d2728e9c6f86da
FChapeau <francois.drouinm@gmail.com>
2014-12-14 19:24:11 -0500

    Began work on game view generation. Navigation broken for now.

--- src/main/java/client/mainView/MainViewController.java
+++ src/main/java/client/mainView/MainViewController.java
@@ -1,6 +1,11 @@
 package client.mainView;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.GridPane;
+
+import javax.annotation.Resources;
+import java.net.URL;
 
 /**
  * Created by boivi_000 on 2014-12-02.
@@ -10,8 +15,26 @@ public class MainViewController {
     int rows;
     int columns;
 
+    @FXML private GridPane GameGrid;
+
     public MainViewController(int rows, int columns) {
         this.rows = rows;
         this.columns = columns;
     }
+
+    @FXML
+    public void initialize(){
+        //TODO Procedural generation of game grid
+
+        for (int i = 0; i < columns-1; i++)
+        {
+            for (int j = 0; j < rows; j++)
+            {
+
+            }
+        }
+        System.out.println("initialize ran!");
+    }
+
+
 }
